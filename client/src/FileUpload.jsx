e0753fbef4ddb0c1c6e9ff7ec6ce7831c0db06ec
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-07-09 20:31:01 +0530

    Update FileUpload.jsx

--- client/src/FileUpload.jsx
+++ client/src/FileUpload.jsx
@@ -9,7 +9,7 @@ const [transaction,setTransaction]=useState("");
              if(file){
                  const formData = new FormData();
                  formData.append("file",file);
-                 const response = await fetch('http://localhost:5000/upload',{
+                 const response = await fetch('https://nft-backend-abwo.onrender.com/upload',{
                      method:'POST',
                      body:formData
                  }).then(response=>response.json())
